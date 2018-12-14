13c1e995ab6e5581d73d165ace4803708c1e6838
nic-chen <4061470@qq.com>
2018-12-14 21:30:37 +0800

    feat jaeger

--- micro/server.go
+++ micro/server.go
@@ -1,17 +1,14 @@
 package micro
 
 import (
-	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-middleware/auth"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	//"github.com/nic-chen/nice/micro/registry"
-	"github.com/nic-chen/nice/micro/tracing"
-	"go.uber.org/zap"
+	//"github.com/nic-chen/nice/micro/tracing"
+	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
+
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -32,37 +29,9 @@ func NewServer(name string, opts ...Option) (*Server, error) {
 		Option: sOptions,
 	}
 
-	uins := []grpc.UnaryServerInterceptor{grpc_ctxtags.UnaryServerInterceptor()}
-	sins := []grpc.StreamServerInterceptor{grpc_ctxtags.StreamServerInterceptor()}
-
-	if sOptions.tracer != nil {
-		uins = append(uins, grpc_opentracing.UnaryServerInterceptor(grpc_opentracing.WithTracer(sOptions.tracer)))
-		sins = append(sins, grpc_opentracing.StreamServerInterceptor(grpc_opentracing.WithTracer(sOptions.tracer)))
-	}
-	if sOptions.logger == nil {
-		sOptions.logger, err = zap.NewDevelopment()
-		if err != nil {
-			return nil, err
-		}
-	}
-	uins = append(uins, grpc_zap.UnaryServerInterceptor(sOptions.logger))
-
-	sins = append(sins, grpc_zap.StreamServerInterceptor(sOptions.logger))
-	grpc_zap.ReplaceGrpcLogger(sOptions.logger)
-	// if tracer is nil then set a id use for log request id
-	if sOptions.logger != nil {
-		uins = append(uins, tracing.UnaryServerInterceptor())
-		sins = append(sins, tracing.StreamServerInterceptor())
-	}
-
-	// tag and tracer must at first
-	sOptions.unaryServerInterceptors = append(uins, sOptions.unaryServerInterceptors...)
-	sOptions.streamServerInterceptors = append(sins, sOptions.streamServerInterceptors...)
-
 	if sOptions.prometheus {
 		sOptions.applyOption(WithUnaryServerInterceptor(grpc_prometheus.UnaryServerInterceptor))
 		sOptions.applyOption(WithStreamServerInterceptor(grpc_prometheus.StreamServerInterceptor))
-
 	}
 
 	if sOptions.authFunc != nil {
@@ -72,23 +41,24 @@ func NewServer(name string, opts ...Option) (*Server, error) {
 
 	if sOptions.recovery == nil {
 		sOptions.recovery = RecoveryWithLogger(sOptions)
+		sOptions.applyOption(WithUnaryServerInterceptor(grpc_recovery.UnaryServerInterceptor(
+			grpc_recovery.WithRecoveryHandler(sOptions.recovery))))
+		sOptions.applyOption(WithStreamServerInterceptor(grpc_recovery.StreamServerInterceptor(
+			grpc_recovery.WithRecoveryHandler(sOptions.recovery))))
 	}
-
-	sOptions.applyOption(WithUnaryServerInterceptor(grpc_recovery.UnaryServerInterceptor(
-		grpc_recovery.WithRecoveryHandler(sOptions.recovery))))
-	sOptions.applyOption(WithStreamServerInterceptor(grpc_recovery.StreamServerInterceptor(
-		grpc_recovery.WithRecoveryHandler(sOptions.recovery))))
 
 	return srv, err
 }
 
 func (t Server) BuildGrpcServer() *grpc.Server {
 	var opts = t.Option.grpcOptions
-	if len(t.Option.unaryServerInterceptors) > 0 {
+
+	if t.Option.tracer != nil {
 		opts = append(opts, grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(t.Option.unaryServerInterceptors...),
+			otgrpc.OpenTracingServerInterceptor(t.Option.tracer),
 		))
 	}
+
 	rpcSrv := grpc.NewServer(opts...)
 	return rpcSrv
 }
